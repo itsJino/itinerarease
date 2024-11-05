68bdc7fe5b469cb158967a0a6943000c09b8e3b5
joshuaflores <floresjoshua2603@gmail.com>
2024-11-05 20:43:46 +0000

    Update project name

--- itinerarease/itinerarease/asgi.py
+++ itinerarease/itinerarease/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for geodjango_tutorial project.
+ASGI config for itinerarease project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geodjango_tutorial.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'itinerarease.settings')
 
 application = get_asgi_application()
